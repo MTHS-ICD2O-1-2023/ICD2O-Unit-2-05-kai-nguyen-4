457fba6c2428ea4c065421404732a3b8e507129c
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-25 18:20:18 +0000

    web made for income

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function findIncomeClicked() {
+  const hoursInput = parseInt(document.getElementById("hours-entered").value)
+  const incomeInput = parseInt(document.getElementById("income-entered").value)
+  const govTax = 0.18 * (hoursInput * incomeInput) 
+  const totalEarnings = (hoursInput * incomeInput) - govTax
+  document.getElementById("answer").innerHTML = "Your total income is: $" + totalEarnings.toFixed(2) + "<br>" + "The government will take: $" + govTax.toFixed(2) 
+}
